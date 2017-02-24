698029dbcd79a08c3764535e9a8df1f01a6793f5
levifussell <levifussell@hotmail.com>
2017-02-24 07:50:33 +0000

    NLP question classification complete

--- Website/Python_connects/dbTest.py
+++ Website/Python_connects/dbTest.py
@@ -1,5 +1,47 @@
-#!/usr/bin/python
+# !/usr/bin/python
 import MySQLdb
+import questionProcessing as qp
+
+def computeScoredQuestions(questions):
+    scoredQuestions = {}
+    tfidfQuestions = {}
+    tfidfQ = qp.TFIDF(questions)
+    for q in questions:
+        if scoredQuestions.has_key(q):
+            scoredQuestions[q] += 1
+        else:
+            scoredQuestions[q] = 1
+            tfidfQuestions[q] = tfidfQ[questions.index(q)]
+    # print("h:{}".format(scoredQuestions))
+    deletes = True
+    while deletes:
+        deletes = False
+        for q1 in scoredQuestions:
+            for q2 in scoredQuestions:
+                if q1 != q2:
+                    # print(q1)
+                    # print(q2)
+                    # isSame = qp.classify(q1, q2)
+                    isSame = qp.cosSim(tfidfQuestions[q1], tfidfQuestions[q2])
+                    # print(isSame)
+                    if isSame:
+                        # deletedQs.append(q2)
+                        scoredQuestions[q2] += scoredQuestions[q1]
+                        print(scoredQuestions[q2])
+                        scoredQuestions[q1] += scoredQuestions[q2]
+                        del scoredQuestions[q1]
+                        deletes = True
+                if deletes:
+                    break
+            if deletes:
+                break
+
+        # for d in deletedQs:
+            # del scoredQuestions[d]
+
+    # print(scoredQuestions)
+
+    return scoredQuestions
 
 db = MySQLdb.connect(host="tyr.czavorwfa0ij.eu-west-2.rds.amazonaws.com",    # your host, usually localhost
                      user="admin",         # your username
@@ -14,8 +56,13 @@ cur = db.cursor()
 cur.execute("SELECT text_message FROM texts")
 
 # print all the first cell of all the rows
+allRecentQuestions = []
 for row in cur.fetchall():
-    print "{}".format(row[0])
-
+    # print "{}".format(row[0])
+    allRecentQuestions.append(row[0])
 
 db.close()
+
+scoredQuestions = computeScoredQuestions(allRecentQuestions)
+# NOW WE HAVE THE QUESTIONS SCORED, WE WANT TO PUSH THEM TO THE DATABASE
+
